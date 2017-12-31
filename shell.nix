975638e8c4c622134e4a685c12660727fc1b8bf3
Erik Schnetter <schnetter@gmail.com>
2017-12-31 14:35:56 -0500

    Experiment with Nix

---
+++ shell.nix
@@ -0,0 +1,29 @@
+with (import <nixpkgs> {});
+
+let
+  # MUST match resolver in stack.yaml
+  resolver = haskell.packages.lts-10_1.ghc;
+
+  native_libs = [
+    # libffi
+    # zlib
+    # ncurses
+    # gmp
+    # pkgconfig
+  ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    Cocoa
+    CoreServices
+  ]);
+
+in stdenv.mkDerivation {
+
+  name = "wavetoy2BuildEnv";
+
+  buildInputs = [ resolver ] ++ native_libs;
+
+  STACK_IN_NIX_EXTRA_ARGS = builtins.foldl'
+    (acc: lib:
+      # " --extra-lib-dirs=${lib}/lib --extra-include-dirs=${lib}/include" + acc)
+      acc)
+    "" native_libs;
+}
